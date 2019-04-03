11a20b2db168abe292ad808d73aabf07617def67
Chris2552 <nurmiaho@hotmail.com>
2019-04-03 18:24:08 +0300

    Full Stack app.

---
+++ build/precache-manifest.dca736ddcb75ad89cf1038170697f1b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "72cd511310ee1d5e78b5",
+    "url": "/static/js/main.72cd5113.chunk.js"
+  },
+  {
+    "revision": "06610d875ff91dd014c9",
+    "url": "/static/js/2.06610d87.chunk.js"
+  },
+  {
+    "revision": "92b737d223064114346a532f390225f4",
+    "url": "/index.html"
+  }
+];
